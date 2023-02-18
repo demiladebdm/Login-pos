eaf26fce47a6ae3faff68e37e48ae791d5a1be8b
danielmordi <danielmordi@gmail.com>
2023-02-18 18:06:06 +0100

    first page

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -10,11 +10,14 @@ const Register = () => {
 
   return (  
     <div className='register'>
-        {/* <Link to='/'>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png" alt="" className='register__logo' />
-        </Link> */}
+      
 
         <div className="register__header">
+          <div className="logo">
+              <Link to='/' className='logoLink'>
+                <h1 className='logoHeader'>M</h1>
+              </Link>
+          </div>
           <div className="register__headerText">
             <h1>Welcome back,</h1>
             <h1>Eag#####</h1>
